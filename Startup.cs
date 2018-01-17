d2047a48a5f48228581bdaf535f48f8307a12fd6
Andre Broers <Andre.Broers@ict.nl>
2018-01-17 21:06:11 +0100

    blog post

---
+++ Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Com.Bekijkhet.MySwashbuckleAADWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Audience = "< webapiresourceid >";
+                    options.Authority = "https://login.windows.net/< tenantid >";
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("oauth2", new OAuth2Scheme
+                {
+                    Type = "oauth2",
+                    Flow = "implicit",
+                    AuthorizationUrl = "https://login.microsoftonline.com/< tenantid >/oauth2/authorize",
+                    Scopes = new Dictionary<string, string>
+                    {
+                        { "read", "Access read operations" },
+                        { "write", "Access write operations" }
+                    },
+                    TokenUrl = "https://login.microsoftonline.com/< tenantid >/oauth2/token"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.ConfigureOAuth2("< clientappid >", "< clientappkey >", "< webapiresourceid >", "mywebapi", " ", additionalQueryStringParameters: new Dictionary<string, string>
+                {
+                    { "resource", "< webapiresourceid >" }
+                });
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
